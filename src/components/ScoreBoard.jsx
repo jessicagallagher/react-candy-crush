0a18e9c633074b9a2e2f00c24b94bc2315859edd
jessicagallagher <jessicalyngallagher@me.com>
2022-11-16 16:37:15 -0500

    complete basic game and start on touch screen

---
+++ src/components/ScoreBoard.jsx
@@ -0,0 +1,15 @@
+import girl from '../images/candy-crush-girl.png';
+
+export default function ScoreBoard({ score }) {
+  return (
+    <div className='main'>
+      <div className='icon'>
+        <img src={girl} alt='' className='girl' />
+        <div className='score-board'>
+          <h2 className='score-header'>Score:</h2>
+          <p className='score'>{score}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
